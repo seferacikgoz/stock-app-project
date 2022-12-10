fcda744119c8fab65d0c0a1a9acb0ead8b131080
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-12-10 13:16:44 +0300

    icons2

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -48,13 +48,13 @@ const MenuListItems = () => {
   return (
     <div>
     <List>
-    {[].map((text, index) => (
-      <ListItem key={text} disablePadding>
+    {icons?.map((item, index) => (
+      <ListItem key={index} disablePadding>
         <ListItemButton>
           <ListItemIcon>
-            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+          {item.icon}
           </ListItemIcon>
-          <ListItemText primary={text} />
+          <ListItemText primary={item.title} />
         </ListItemButton>
       </ListItem>
     ))}
